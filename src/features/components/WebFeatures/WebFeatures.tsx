9dadf6483da624b2f935e1c1c38218422de4d8ae
Helene T <helene.teychene@gmail.com>
2026-01-04 19:43:38 +0100

    feat/implemented + and - buttons to increase page and language values

--- src/features/components/WebFeatures/WebFeatures.tsx
+++ src/features/components/WebFeatures/WebFeatures.tsx
@@ -1,4 +1,5 @@
 import type { WebFeatures } from "../../types";
+import "./WebFeatures.css";
 
 export function WebFeatures({ pages, setPages, languages, setLanguages }: WebFeatures) {
 
@@ -14,15 +15,27 @@ export function WebFeatures({ pages, setPages, languages, setLanguages }: WebFea
     return (
         <>
             <div className="p-0.5 pt-1 col-span-full text-right">
-                <label>
+                <label className="flex gap-1 items-center justify-end">
                     Pages:
-                    <input type="number" className="bg-grey p-[5px] ml-1 rounded-sm" min="1" value={pages} onChange={(e) => addPages(e)} />
+                    <button className="btn-plus-minus"
+                        onClick={() => {
+                            if (pages <= 1) setPages(1);
+                            else setPages(pages - 1)
+                        }}>-</button>
+                    <input type="number" className="web-feat-inputs" min="1" value={pages} onChange={(e) => addPages(e)} />
+                    <button className="btn-plus-minus" onClick={() => { setPages(pages + 1) }}>+</button>
                 </label>
             </div>
             <div className="p-0.5 col-span-full text-right">
-                <label>
+                <label className="flex gap-1 items-center justify-end">
                     Languages:
-                    <input type="number" className="bg-grey p-[5px] ml-1 rounded-sm" min="1" value={languages} onChange={(e) => addLanguages(e)} />
+                    <button className="btn-plus-minus"
+                        onClick={() => {
+                            if (languages <= 1) setLanguages(1);
+                            else setLanguages(languages - 1)
+                        }}>-</button>
+                    <input type="number" className="web-feat-inputs" min="1" value={languages} onChange={(e) => addLanguages(e)} />
+                    <button className="btn-plus-minus" onClick={() => { setLanguages(languages + 1) }}>+</button>
                 </label>
             </div>
         </>
