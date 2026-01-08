84cf7656b62bc9d1ba520a697e1fa59c9fc65357
Helene T <helene.teychene@gmail.com>
2026-01-08 23:29:55 +0100

    feature/ added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import { HomePage } from "./pages/HomePage";
+import { PricingPage } from "./pages/PricingPage";
 
 function App() {
 
   return (
     <Routes>
       <Route index element={<HomePage />} />
+      <Route path="pricing" element={<PricingPage />} />
     </Routes>
   )
 }
