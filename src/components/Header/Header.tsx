1e342cc5d51b21e744f287c98146c257f59e6d80
Helene T <helene.teychene@gmail.com>
2026-01-16 20:23:38 +0100

    refactor / moved the header from features>calculator to shared component, as that header would be reused in other pages if any

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import Logo from "../../assets/logo.png";
+import { Navbar } from "../Navbar/Navbar";
+
+export function Header() {
+    return (
+        <header className="w-screen flex relative z-10">
+            <img src={Logo} alt="Sasha Web services"
+                className="h-[30vw] sm:h-[22vw] md:h-[13vw] absolute z-20 left-0 sm:left-4" />
+            <Navbar bg="colorful" />
+        </header>
+    )
+}
